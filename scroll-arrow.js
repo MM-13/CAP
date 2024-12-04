a1ea591ed28fdd6fed26c04cfe050d781b038651
MM-13 <94961568+MM-13@users.noreply.github.com>
2024-12-04 01:08:52 +0100

    Create scroll-arrow.js

---
+++ scroll-arrow.js
@@ -0,0 +1,8 @@
+document.addEventListener("scroll", function() {
+    var arrow = document.getElementById("scroll-arrow");
+    if (window.scrollY > 0) {
+        arrow.classList.add("hidden");
+    } else {
+        arrow.classList.remove("hidden");
+    }
+});
