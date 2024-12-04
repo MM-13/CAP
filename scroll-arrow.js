bb29e9edba2665ba3e27f121027d64b40294d260
MM-13 <94961568+MM-13@users.noreply.github.com>
2024-12-04 01:13:40 +0100

    Update scroll-arrow.js

--- scroll-arrow.js
+++ scroll-arrow.js
@@ -1,4 +1,4 @@
-document.addEventListener("scroll", function() {
+window.addEventListener("scroll", function() {
     var arrow = document.getElementById("scroll-arrow");
     if (window.scrollY > 0) {
         arrow.classList.add("hidden");
